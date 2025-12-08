9cb2f2266f76d3b3c105fd9d17a53fccfc4638f9
lookslikeameya <ameya7072@gmail.com>
2025-12-09 01:16:00 +0530

    connected postgres serveer to django....also registered models on admin page

--- backend/settings.py
+++ backend/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'project_db',
+        'USER': 'eventuser',
+        'PASSWORD': '021007',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
