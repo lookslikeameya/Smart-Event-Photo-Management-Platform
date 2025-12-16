21ab43da8dda6cbc20b3e6dd4fffac6d691bd687
Ameya Gupta <ameya7072@gmail.com>
2025-12-16 19:08:35 +0530

    front end created..

--- backend/settings.py
+++ backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'albums',
     'photos',
     'comments',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -52,7 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
+CORS_ALLOW_ALL_ORIGINS= True
 
 ROOT_URLCONF = 'backend.urls'
 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import Login from "./pages/Login.jsx"
+
+function App() {
+  return (
+    <Login />
+  )
+}
+
+export default App
