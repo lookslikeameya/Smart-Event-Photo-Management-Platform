4135493628227dfdbe96c1862aaaca1d151100ee
Ameya Gupta <ameya7072@gmail.com>
2025-12-16 16:59:29 +0530

    created basic login page and connected to my backend api for login and stored JWT in local storage....also removed CORS for now

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import "./Login.css";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  try {
+    const response = await fetch("http://127.0.0.1:8000/api/accounts/login/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      alert(data.non_field_errors || "Login failed");
+      return;
+    }
+
+    // SAVE TOKENS
+    localStorage.setItem("access", data.access);
+    localStorage.setItem("refresh", data.refresh);
+
+    alert("Login successful!");
+  } catch (error) {
+    console.error(error);
+    alert("Server error");
+  }
+};
+
+
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Login</h2>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
